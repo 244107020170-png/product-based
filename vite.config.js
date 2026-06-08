a0ef9bb1bd51b6926076613c04a19639bcd1c4f3
244107020170-png <244107020170@student.polinema.ac.id>
2026-06-08 14:17:15 +0700

    add all css js assets to vite input

--- vite.config.js
+++ vite.config.js
@@ -5,31 +5,34 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-
-                'resources/css/player-dashboard.css',
-                'resources/css/player-profile.css',
-                'resources/css/player-profile-view.css',
-                'resources/css/player-history.css',
-
-                'resources/css/owner-dashboard.css',
-                'resources/css/owner-kelola-booking.css',
-                'resources/css/owner-jadwal-slot.css',
-                'resources/css/owner-form-field.css',
-                'resources/css/owner-bookings.css',
-                'resources/css/pemeliharaanDanKontrol.css',
-                'resources/css/owner-pengaturan.css',
-
-                'resources/css/pages.css',
+                // CSS
                 'resources/css/admin.css',
+                'resources/css/app.css',
                 'resources/css/choose-role.css',
+                'resources/css/owner-bookings.css',
+                'resources/css/owner-dashboard.css',
+                'resources/css/owner-form-field.css',
+                'resources/css/owner-jadwal-slot.css',
+                'resources/css/owner-kelola-booking.css',
+                'resources/css/owner-pengaturan.css',
+                'resources/css/pages.css',
+                'resources/css/pemeliharaanDanKontrol.css',
+                'resources/css/player-dashboard.css',
+                'resources/css/player-history.css',
+                'resources/css/player-profile-view.css',
+                'resources/css/player-profile.css',
+                'resources/css/register.css',
 
+                // JS
+                'resources/js/admin.js',
                 'resources/js/app.js',
+                'resources/js/auth.js',
+                'resources/js/bootstrap.js',
+                'resources/js/choose-role.js',
+                'resources/js/owner-kelola-booking.js',
                 'resources/js/player-dashboard.js',
                 'resources/js/player-history.js',
-                'resources/js/owner-kelola-booking.js',
-                'resources/js/admin.js',
-                'resources/js/choose-role.js',
+                'resources/js/register.js',
             ],
             refresh: true,
         }),
