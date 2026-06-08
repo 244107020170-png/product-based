dd344800123b299ecdded1b0702785e05686397b
244107020170-png <244107020170@student.polinema.ac.id>
2026-06-08 14:04:38 +0700

    add choose role assets to vite build

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
+
                 'resources/css/player-dashboard.css',
                 'resources/css/player-profile.css',
                 'resources/css/player-profile-view.css',
                 'resources/css/player-history.css',
+
                 'resources/css/owner-dashboard.css',
                 'resources/css/owner-kelola-booking.css',
                 'resources/css/owner-jadwal-slot.css',
@@ -17,13 +19,17 @@ export default defineConfig({
                 'resources/css/owner-bookings.css',
                 'resources/css/pemeliharaanDanKontrol.css',
                 'resources/css/owner-pengaturan.css',
+
                 'resources/css/pages.css',
+                'resources/css/admin.css',
+                'resources/css/choose-role.css',
+
                 'resources/js/app.js',
                 'resources/js/player-dashboard.js',
                 'resources/js/player-history.js',
                 'resources/js/owner-kelola-booking.js',
-            'resources/css/admin.css',
-            'resources/js/admin.js',
+                'resources/js/admin.js',
+                'resources/js/choose-role.js',
             ],
             refresh: true,
         }),
